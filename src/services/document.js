1afbec2414616530bfcd59bb30a377e8a4ed3e8e
Husain <hussainalmajed.2005@gmail.com>
2026-08-29 19:35:13 +0300

    crete document service function

---
+++ src/services/document.js
@@ -0,0 +1,23 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/documents`
+
+const create = async (documentData) => {
+    try {
+        const res = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(documentData),
+        })
+
+        const data = await res.json()
+        return data
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+export {
+    create,
+}
