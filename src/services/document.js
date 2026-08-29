bd7efd8496032b7d8e11b9d7c76a11c0ab7c7298
Husain <hussainalmajed.2005@gmail.com>
2026-08-29 19:38:52 +0300

    index document service function

--- src/services/document.js
+++ src/services/document.js
@@ -18,6 +18,21 @@ const create = async (documentData) => {
     }
 }
 
+const index = async () => {
+    try {
+        const res = await fetch (BASE_URL, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+        })
+
+        return res.json()
+    } catch(error) {
+        console.log(error)
+    }
+}
+
 export {
     create,
+    index,
 }
