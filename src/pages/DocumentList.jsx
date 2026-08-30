af745a7db52760f03766683091106eb24ee7ff76
Husain <hussainalmajed.2005@gmail.com>
2026-08-31 00:33:36 +0300

    Created the documentList page

--- src/pages/DocumentList.jsx
+++ src/pages/DocumentList.jsx
@@ -1,15 +1,46 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router"
+import * as documentServices from "../services/document"
 
 const DocumentList = () => {
 
-    const { agreementId } = useParams
+    const { agreementId } = useParams()
     const [documents , setDocuments] = useState([])
     const [loading , setLoading] = useState(true)
 
+    useEffect(() => {
+        const fetchDocuments = async () => {
+            const allDocuments = await documentServices.index()
+            const agreementDocuments = allDocuments.filter((document) => {
+                return String(document.agreement?._id) === String(agreementId)
+            })
+
+            setDocuments(agreementDocuments)
+            setLoading(false)
+        }
+        fetchDocuments()
+    }, [agreementId])
+
+    if (loading) {
+        return <h2>Loading documents...</h2>
+    }
 
     return (
-        <h1>Document List</h1>
+        <div>
+            <h1>Document List</h1>
+
+            {documents.length === 0 ? (<p>No documents added yet</p>) : (
+                <ul>
+                    {documents.map((document) => (
+                        <li key={document._id}>
+                            <p>{document.title}</p>
+                            <p>{document.documentType}</p>
+                            <a href="{document.url}">View document</a>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     )
 
 }
