c8972bc45575ccbaac19125126e46cf22a3039ae
Husain <hussainalmajed.2005@gmail.com>
2026-08-30 08:35:24 +0300

    create inspection service function

---
+++ src/services/inspection.js
@@ -0,0 +1,23 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/inspections`
+
+const create = async (inspectionData) => {
+    try {
+        const res = await fetch (BASE_URL , {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-type': 'application/json',
+      },
+      body: JSON.stringify(inspectionData),
+        })
+
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    create,
+}
