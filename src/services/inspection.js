4b6250a3d3ae67eb9f780348b8603a49507c64f5
Husain <hussainalmajed.2005@gmail.com>
2026-08-30 08:37:45 +0300

    index inspection service function

--- src/services/inspection.js
+++ src/services/inspection.js
@@ -18,6 +18,21 @@ const create = async (inspectionData) => {
     }
 }
 
+const index = async () => {
+    try {
+        const res = await fetch(BASE_URL , {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+        })
+
+        return res.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export {
     create,
+    index,
 }
