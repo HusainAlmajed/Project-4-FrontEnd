7f3e54098620c37a8281934bf17bdeba40a4aa22
unknown <jonykadhem@gmail.com>
2026-09-01 14:54:10 +0300

    add the claoudenary for the incpection

--- src/pages/DashboardCostumer.jsx
+++ src/pages/DashboardCostumer.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "react-router"
 
+
 const DashboardCostumer = ({ agreements = [], user }) => {
 
     const [loading, setLoading] = React.useState(true)
