d2772c9b96a7acb4bff243b1aabe1cd31373dcee
HusainAlmajed <hussainalmajed.2005@gmail.com>
2026-09-03 01:17:15 +0300

    Created the notFound page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router"
+
+const NotFound = () => {
+    return (
+        <main className="not-found-page">
+            <section className="not-found-content">
+
+                <p className="not-found-label">THIS PAGE DOES NOT EXIST</p>
+
+                <h1>404<span>.</span></h1>
+
+                <h2>Page not found</h2>
+
+                <p className="not-found-description">The page you are looking for may have been moved, deleted, or the address may be incorrect.</p>
+
+                <Link to="/" className="not-found-btn">Back to home →</Link>
+
+            </section>
+        </main>
+    )
+}
+
+export default NotFound
